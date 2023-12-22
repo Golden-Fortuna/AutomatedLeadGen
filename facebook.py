87acccf7590630a99f51dc741dc3d7df5cb2f646
Samuel Hepditch <hepditchsam@gmail.com>
2023-12-22 17:37:02 -0500

    update gui.

--- facebook.py
+++ facebook.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from threading import Thread
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,7 +9,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from enum import Enum
 import time
 import pyautogui
 import pyperclip
@@ -19,9 +16,8 @@ import pyperclip
 class AutomationApp:
     def __init__(self, master):
         self.master = master
-        master.title("Facebook Automation")
-        master.configure(bg='#2C2F33')  # Dark grey background
-
+        master.title("Automated Lead Generation")
+        master.configure(bg='#2C2F33') 
         style = ttk.Style()
         style.theme_use('alt')
         style.configure('TNotebook', background='#2C2F33', borderwidth=0)
@@ -32,16 +28,23 @@ class AutomationApp:
         style.configure('TLabel', background='#2C2F33', foreground='#FFF', font=('Helvetica', 10))
         style.configure('TEntry', fieldbackground='#23272A', foreground='#FFF', borderwidth=0)
         style.map('TEntry', fieldbackground=[('focus', '#23272A')], foreground=[('focus', '#FFF')])
-
         tabControl = ttk.Notebook(master)
         tab1 = ttk.Frame(tabControl)
         tabControl.add(tab1, text='Automation')
         tab2 = ttk.Frame(tabControl)
         tabControl.add(tab2, text='Dashboard')
         tabControl.pack(expand=1, fill="both", padx=10, pady=10)
-
         self.create_automation_tab(tab1)
         self.create_dashboard_tab(tab2)
+
+    def check_fields_filled(self):
+        email = self.email_text.get("1.0", tk.END).strip()
+        password = self.password_text.get("1.0", tk.END).strip()
+        message = self.message_text.get("1.0", tk.END).strip()
+        if email and password and message:
+            self.start_button.config(state="normal")
+        else:
+            self.start_button.config(state="disabled")
 
     def create_automation_tab(self, tab):
         ttk.Label(tab, text="Email:").grid(row=0, column=0, sticky='e', padx=(10, 0), pady=(10, 10))
@@ -53,7 +56,10 @@ class AutomationApp:
         ttk.Label(tab, text="Message:").grid(row=2, column=0, sticky='ne', padx=(10, 0), pady=(0, 10))
         self.message_text = tk.Text(tab, height=10, width=50, bg='#23272A', fg='#FFF', borderwidth=0, insertbackground='#FFF')
         self.message_text.grid(row=2, column=1, padx=(0, 10), pady=(0, 10), sticky='ew')
-        self.start_button = ttk.Button(tab, text="Start Automation", command=self.start_automation)
+        self.email_text.bind("<KeyRelease>", lambda event: self.check_fields_filled())
+        self.password_text.bind("<KeyRelease>", lambda event: self.check_fields_filled())
+        self.message_text.bind("<KeyRelease>", lambda event: self.check_fields_filled())
+        self.start_button = ttk.Button(tab, text="Start Automation", command=self.start_automation, state="disabled")
         self.start_button.grid(row=3, column=1, padx=(0, 10), pady=(0, 10), sticky='ew')
         self.status_label = ttk.Label(tab, text="", foreground='#32CD32')  # Lime green for visibility
         self.status_label.grid(row=4, column=1, padx=(0, 10), pady=(0, 10), sticky='ew')
@@ -81,7 +87,7 @@ class AutomationApp:
                 listing.click()
                 time.sleep(2)
                 try:
-                    element = driver.find_element(By.XPATH, "//span[contains(., 'Message Again')]")
+                    driver.find_element(By.XPATH, "//span[contains(., 'Message Again')]")
                     print("Already Messaged...")
                 except NoSuchElementException:
                     print("Sending Message...")
@@ -91,12 +97,12 @@ class AutomationApp:
                     pyperclip.copy(message)
                     pyautogui.keyDown('command')
                     pyautogui.press('v')
-                    pyautogui.click(1150, 865)
+                    pyautogui.move(1150, 865)
                     pyautogui.click(500,500)
                     messages_sent += 1
+                    self.status_label.config(text=f"{messages_sent} messages sent to potential clients.")
                 ActionChains(driver).send_keys(Keys.ESCAPE).perform()
                 time.sleep(2)
-            return messages_sent
         except Exception as e:
             print("Error encountered:", e)
 
@@ -130,17 +136,15 @@ class AutomationApp:
         time.sleep(3)
         self.open_rentals(driver)
         time.sleep(3)
-        messages_sent = self.send_msgs(driver, message)
+        self.send_msgs(driver, message)
         driver.quit()
-        return messages_sent
 
     def start_automation(self):
         email = self.email_text.get("1.0", tk.END)
         password = self.password_text.get("1.0", tk.END)
         message = self.message_text.get("1.0", tk.END)
         self.start_button.config(state="disabled")
-        messages_sent = self.run_automation(email, password, message)
-        self.status_label.config(text=f"{messages_sent} messages sent to potential clients.")
+        self.run_automation(email, password, message)
         self.start_button.config(state="normal")
 
 
